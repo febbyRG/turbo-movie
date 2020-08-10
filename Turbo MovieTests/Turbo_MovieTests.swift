ea0c673d72cfec9e5a7b9711ca9f3abfa5c6159b
Febby Rachmat Gumilar <febbyrachmatg@gmail.com>
2020-08-10 19:09:42 +0700

    Initial Commit

---
+++ Turbo MovieTests/Turbo_MovieTests.swift
@@ -0,0 +1,34 @@
+//
+//  Turbo_MovieTests.swift
+//  Turbo MovieTests
+//
+//  Created by Febby Gumilar on 10/08/20.
+//  Copyright © 2020 Slash. All rights reserved.
+//
+
+import XCTest
+@testable import Turbo_Movie
+
+class Turbo_MovieTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
